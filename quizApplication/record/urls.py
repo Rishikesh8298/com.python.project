3a729fccea44bd13474b588925811af8229d5882
RishiKesh <rishikesh.karn@outlook.com>
2020-09-23 13:36:14 +0530

    Quiz Application is removed

--- quizApplication/record/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^panel/question/record/$', views.record_list, name = 'record_list'),
-    url(r'^panel/record/del/(?P<pk>\d+)/$', views.record_delete, name ='record_delete'),
-    url(r'^user/panel/record/list/$', views.record_list_user, name = 'record_list_user'),
-    url(r'^user/panel/result/(?P<word>.*)/$', views.user_result, name='user_result'),
-]
--- quizApplication/subject/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^panel/subject/list/$', views.subject_list, name='subject_list'),
-    url(r'^panel/subject/add/$', views.subject_add, name='subject_add'),
-    url(r'^user/panel/quiz/(?P<word>.*)/$', views.user_quiz, name='user_quiz'),
-]
